bc2defb8312ee9a53c214ce9f8b099c31b78d275
Christopher Palmer <snly2386@gmail.com>
2014-11-16 20:04:17 -0800

    init commit'

---
+++ js/_main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+var s = skrollr.init({forceHeight: true})
+
+// $('.kart').jrumble({x: 1, y: 1, rotation: 1})
+// $('.kart').trigger('startRumble')
+
+imagesLoaded('body', function(){
+	console.log('loaded images and shit')
+})
+
+
+
+
+
+
+})
